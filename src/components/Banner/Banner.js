f5ce1f9d2dd18c0aa1bcf9ee5e1373f3b0c2e253
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-11-15 17:41:06 -0300

    Estilização do Banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './Banner.css';
 
-import { getMovies } from '../../Api/TmdbApi';
+import { categories, getMovies } from '../../Api/TmdbApi';
 
 export default function Banner() {
-  const [movie, setMovie] = useState();
+  const [movie, setMovie] = useState({});
 
   const fetchRandomMovie = async () => {
     try {
@@ -36,6 +36,15 @@ export default function Banner() {
         backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         roundPosition: 'center-center',
       }}
-    ></header>
+    >
+      <div className="banner-content">
+        <h1 className="banner-title">{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className="banner-buttons-container">
+          <button className="banner-button">Assistir</button>
+          <button className="banner-button">Minha Lista</button>
+        </div>
+        <div className="banner-description"></div>
+      </div>
+    </header>
   );
 }
